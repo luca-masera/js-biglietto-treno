3f69618b97a1bc2e61ee2e2a6deeaf33388c29a2
luca-masera <lucamasera1995@gmail.com>
2023-10-09 15:11:02 +0200

    if

--- javascript/script.js
+++ javascript/script.js
@@ -7,4 +7,33 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
 
+const euroAlKm = 0.21;
 
+
+let costoBase = euroAlKm * 100;
+console.log(costoBase);
+
+
+
+let numeroKm = prompt('Quanti chilometri vuoi percorrere?');
+let etàPasseggero = prompt('Quanti anni hai?');
+console.log(numeroKm,etàPasseggero);
+
+let biglietto = euroAlKm * numeroKm; 
+
+
+if(etàPasseggero < 18){
+    biglietto = biglietto - (biglietto * 20/100)
+    console.log(biglietto)
+}
+else if(etàPasseggero > 65){
+    biglietto = biglietto - (biglietto * 40/100)
+    console.log(biglietto)
+}
+else((etàPasseggero >= 18) || (etàPasseggero <= 65)) {
+    biglietto = biglietto
+    console.log(biglietto)
+}
+
+let costoBiglietto = document.getElementById ('prezzo-biglietto');
+costoBiglietto.innerHTML = biglietto
